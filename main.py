180e38ed77bff13ed1d17e53d3bfe0781783dbf8
purple(?) <akirathedarktempest@gmail.com>
2026-03-01 17:13:03 +0530

    Add password generator in main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+text="abcdefghigklmnopqrstuvwxyz1234567890!@#$%^&*()"
+l=[]
+for i in text:
+    l.append(i)
+ques=int(input("Number characters of your password: \n"))
+pw=""
+for i in range(ques):
+    a=random.choice(l)
+    pw+=a
+
+print(f"Your password: {pw}")
