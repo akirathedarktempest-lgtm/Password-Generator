47b6b80d97291c5af01247c718e7441f82191c58
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-08-01 15:33:32 +0530

    Add token generator function
    Implement a token generator that creates a secure token using letters, numbers, and special symbols.

---
+++ token/tokenGenerator.py
@@ -0,0 +1,17 @@
+import random
+
+text="abcdefghijklmnopqrstuvwxyz"
+captial_text="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers="0123456789"
+special_symbols="@#$&%"
+
+def generate():
+    token=""
+    for _ in range(3):
+        for __ in range(12):
+            token+=random.choice([random.choice(text),random.choice(captial_text),random.choice(numbers),random.choice(special_symbols)])
+        token+="."
+    return token
+
+print(generate())
+#so...i am working on an application, and that application needs api, and api needs security, and for that, i am making this token generator
